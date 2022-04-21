819b27f9afb2cb136e886ff4f193e268cbb05a50
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-04-21 13:54:06 +0200

    creation fct mouvement

--- fourmi.py
+++ fourmi.py
@@ -31,7 +31,7 @@ HAUTEUR_CASE = HAUTEUR // N
 L_f = LARGEUR_CASE // 3
 H_f = HAUTEUR_CASE
 
-#########################################
+##############################
 # variables globales en plus des widgets
 
 # objets graphiques représentant la grille dans un tableau 2D
@@ -40,14 +40,17 @@ grille = None
 # pour tenir compte des bords
 config_cur = None
 
+liste_couleur =["white", "black"]
+coul = 0
+
 ############################
 # fonction
 def init_grille():
     """Retourne une grille carrée vide
        dimension N, les éléments de la configuration vont de 1 à N
+       //!\\ ne pas autoriser configuration avec 4 case de meme couleur sinon tourne en boucle //!\\
     """
-    global grille, config_cur    
-    liste_couleur =["white", "black"]
+    global grille, config_cur
     grille = [[0 for i in range(N+1)] for j in range(N+1)]
     config_cur = [[0 for i in range(N+1)] for j in range(N+1)]
     for i in range(1, N+1):
@@ -64,12 +67,33 @@ def init_grille():
 
 
 def fourmi():
+    """fait apparaitre la fourmi dans la premiere case à gauche du tableau"""
     global fourmi
     x = 0
     y = 0
     fourmi = canvas.create_rectangle((x+L_f,y), (x+2*L_f,y+HAUTEUR_CASE),
                                     fill="blue")
 
+def mouvement():
+    """- fait tourner la fourmi de 90° (N<-Gauche / B->Droite)
+    - change de couleur case
+    - deplace d'un case la fourmi"""
+    global coul
+    x, y, x0,y0 = canvas.coords(fourmi)
+    i = int((x - L_f)//LARGEUR_CASE)
+    j = int(y//HAUTEUR_CASE)
+
+    if canvas.itemconfigure(grille[i][j]) == 'white': #autre pensee : grille[i][j]['fill'] == 'white' -> ne marche pas
+        coul = 1-coul
+        canvas.itemconfigure(grille[i][j], fill=liste_couleur[coul])
+        canvas.move(fourmi, LARGEUR_CASE, 0)
+
+    if canvas.itemconfigure(grille[i][j]) =='black':
+        coul = 1-coul
+        canvas.itemconfigure(grille[i][j], fill=liste_couleur[coul])        
+        canvas.move(fourmi, -LARGEUR_CASE, 0)
+    
+    canvas.after(20, mouvement)
 
 ############################
 # programme principal
@@ -79,7 +103,7 @@ racine.title("Fourmi de Langton")
 
 # définition des widgets
 canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
-bouton_play = tk.Button(racine, text='play')
+bouton_play = tk.Button(racine, text='play', command=mouvement)
 
 init_grille()
 
