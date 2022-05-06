e82db0c3767a25e30ffb712e2cca59ecdff13879
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-05-06 15:21:44 +0200

    maj

--- fourmi.py
+++ fourmi.py
@@ -20,25 +20,18 @@ import random as rd
 #########################
 
 # taille de la grille carrée
-N = 4
+N = 10
 # dimensions du canvas et de la grille
 LARGEUR = 500
 HAUTEUR = 500
 LARGEUR_CASE = LARGEUR // N
 HAUTEUR_CASE = HAUTEUR // N
 
-# dimensions de la fourmi
-L_f = LARGEUR_CASE // 3
-H_f = HAUTEUR_CASE
-
 ##############################
 # variables globales en plus des widgets
 
 # objets graphiques représentant la grille dans un tableau 2D
 grille = None
-# configuration courante dans un tableau 2D de dimension N+2
-# pour tenir compte des bords
-config_cur = None
 
 liste_couleur =["white", "black"]
 coul = 0
@@ -46,25 +39,22 @@ coul = 0
 ############################
 # fonction
 def init_grille():
-    """Retourne une grille carrée vide
-       dimension N, les éléments de la configuration vont de 1 à N
-       //!\\ ne pas autoriser configuration avec 4 case de meme couleur sinon tourne en boucle //!\\
-           
+    """initialise la grille carrée de dimension N aleatoirement
     """
-    global grille, config_cur
+    global grille
     grille = [[0 for i in range(N+1)] for j in range(N+1)]
-    config_cur = [[0 for i in range(N+1)] for j in range(N+1)]
     for i in range(0, N):
         x = i * LARGEUR_CASE
         for j in range(0, N):
             y = j * HAUTEUR_CASE
             n = rd.randint(0,1)
-            couleur = liste_couleur[n]
-            carre = canvas.create_rectangle(x, y, x+LARGEUR_CASE,
-                                            y+HAUTEUR_CASE, fill=couleur,
-                                            outline="grey50")
+            carre = canvas.create_rectangle((x, y), (x+LARGEUR_CASE, y+HAUTEUR_CASE),
+                                            fill=liste_couleur[n], outline="grey50")
             grille[i][j] = carre
-    fourmi()
+            fic = open("config.txt", "w")
+            fic.write(str(carre)+" ")
+            fic.close()
+    c_fourmi()
 
 
 def c_fourmi():
@@ -73,7 +63,7 @@ def c_fourmi():
     global fourmi
     x = 0
     y = 0
-    fourmi = canvas.create_rectangle((x+L_f,y), (x+2*L_f,y+HAUTEUR_CASE),
+    fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x,y+HAUTEUR_CASE),
                                     fill="blue")
 
 def mouvement():
@@ -81,9 +71,9 @@ def mouvement():
     - change de couleur case
     - deplace d'une case la fourmi"""
     global coul
-    x, y, x0,y0 = canvas.coords(fourmi)
-    i = int((x - L_f)//LARGEUR_CASE)
-    j = int(y//HAUTEUR_CASE)
+    x0,y0, x1,y1 = canvas.coords(fourmi)
+    i = int(x0//LARGEUR_CASE)
+    j = int(y0//HAUTEUR_CASE)
 
     if canvas['bg'] == 'white': # REECUPERER COULEUR (CONFIGURATION DE LA GRILLE) DANS UN FICHIEEEEER
         coul = 1-coul
@@ -101,7 +91,7 @@ def mouvement():
     tore()
 
 def tore():
-    """la fourmi passe de l'autre coté du canvas"""
+    """la fourmi passe de l'autre coté du canvas quand elle atteint un bord"""
     x0, y0, x1, y1 = canvas.coords(fourmi)
     if x1<0:
         canvas.coords(fourmi, x0+LARGEUR,y0, x1+LARGEUR, y1)
@@ -112,6 +102,15 @@ def tore():
     if y0>HAUTEUR:
         canvas.coords(fourmi, x0,y0-HAUTEUR, x1, y1-HAUTEUR)
 
+def pause():
+    """permet d'arreter le programme"""
+    pass
+
+def lecture_fichier():
+    fic = open("config.txt", "r")
+    for ligne in fic:
+        id = ligne.split()
+
 ############################
 # programme principal
 
@@ -121,6 +120,7 @@ racine.title("Fourmi de Langton")
 # définition des widgets
 canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
 bouton_play = tk.Button(racine, text='play', command=mouvement)
+bouton_pause = tk.Button(racine, text='pause', command=pause)
 
 init_grille()
 
