194d9eca4b4e109a66669d3e3706765db548c1c9
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-05-09 00:32:53 +0200

    creation fct compteur + update readme

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ VOLIVERT Coline
 https://github.com/uvsq22102656/projet_fourmi_langton
 
 #########################################
+
+REGLES DU JEU :
+La fourmi de Langton est représentée par une fleche bleue la pointe indiquant son orientation. Elle se deplace sur un tableau initalement blanc.
+Elle part du centre, orientée au nord.
+
+On peut :
+- mettre en marche l'automate cellulaire en appuyant sur le bouton "play"
+- mettre en pause le deroulement des étapes en appuyant sur le bouton "pause"
+- avancer une étape par étape en appuyant sur ">>"
+- revenir une etape en arriere en appuyant sur "<<"
+- accélérer ou ralentir la vitesse de passage des étapes avec "accelerer" et "ralentir"
+- sauvegarder et charger une ancienne configuration avec "sauvegarder" et "ancienne configuration"
--- fourmi.py
+++ fourmi.py
@@ -34,8 +34,8 @@ vitesse = 1000
 ##############################
 # variables globales en plus des widgets
 fourmi = None
-p = True
-compt = 0 #variable pour relancer le jeu apres avoir mis sur pause
+p = True #variable pour relancer le jeu apres avoir mis sur pause
+cmp = 0 #compte le nombre d'étapes réalisées
 
 #position initiale au milieu du canvas orienté Nord
 X = LARGEUR_CASE*N//2
@@ -51,16 +51,28 @@ def creation_case(i,j):
     x = j*LARGEUR_CASE
     y = i*HAUTEUR_CASE
     case = canvas.create_rectangle((x,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE),
-                                    fill='black')
+                                    fill='grey25')
     return case
 
-def c_fourmi():
+def c_fourmi(x, y, dir):
     """- cree la liste id à la bonne taille
-    - fait apparaitre la fourmi au milieu du tableau"""
+    - fait apparaitre la fourmi au milieu du tableau, orientée Nord"""
     global id, fourmi
     id = [[0]*(N) for k in range(N)] #initialisation de la grille blanche = 0
-    fourmi = canvas.create_polygon((X+LARGEUR_CASE//2,Y), (X+LARGEUR_CASE,Y+HAUTEUR_CASE), (X,Y+HAUTEUR_CASE),
-                                    fill="blue")
+
+    if dir == "N":
+        fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x,y+HAUTEUR_CASE),
+                                    fill='steelblue3')
+    elif dir == "E":
+        fourmi = canvas.create_polygon((x+LARGEUR_CASE,y+HAUTEUR_CASE//2), (x,y), (x,y+HAUTEUR_CASE),
+                                            fill='steelblue3')
+    elif dir == "S":
+        fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y+HAUTEUR_CASE), (x,y), (x+LARGEUR_CASE,y),
+                                            fill='steelblue3')
+    elif dir == "O":
+        fourmi = canvas.create_polygon((x,y+HAUTEUR_CASE//2), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x+LARGEUR_CASE,y),
+                                            fill='steelblue3')
+
 
 def mouvement(pos, dir, id):
     """- fait tourner la fourmi de 90° (N<-Gauche / B->Droite)
@@ -75,83 +87,54 @@ def mouvement(pos, dir, id):
             if j == N:
                 j=0 
             dir = "E"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x+LARGEUR_CASE,y+HAUTEUR_CASE//2), (x,y), (x,y+HAUTEUR_CASE),
-                                            fill='blue')
+
         elif dir == "S":
             j -= 1
             if j<0:
                 j=N-1
             dir = "O"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x,y+HAUTEUR_CASE//2), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x+LARGEUR_CASE,y),
-                                            fill='blue')
+
         elif dir == "E":
             i += 1
             if i==N :
                 i=0 
             dir = "S"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y+HAUTEUR_CASE), (x,y), (x+LARGEUR_CASE,y),
-                                            fill='blue')
+
         elif dir == "O":
             i -= 1
             if i<0:
                 i=N-1
             dir = "N"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x,y+HAUTEUR_CASE),
-                                            fill='blue')
+            
     else:
         if dir == "S":
             j += 1
             if j==N:
                 j=0 
             dir = "E"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x+LARGEUR_CASE,y+HAUTEUR_CASE//2), (x,y), (x,y+HAUTEUR_CASE),
-                                            fill='blue')
+            
         elif dir == "N":
             j -= 1
             if j<0:
                 j=N-1
             dir = "O"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x,y+HAUTEUR_CASE//2), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x+LARGEUR_CASE,y),
-                                            fill='blue')
+            
         elif dir == "O":
             i += 1
             if i==N:
                 i=0 
             dir = "S"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y+HAUTEUR_CASE), (x,y), (x+LARGEUR_CASE,y),
-                                            fill='blue')
+
         elif dir == "E":
             i -= 1
             if i<0:
                 i=N-1
             dir = "N"
-            x = j*LARGEUR_CASE
-            y = i*HAUTEUR_CASE
-            canvas.delete(fourmi)
-            fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x,y+HAUTEUR_CASE),
-                                            fill='blue')
 
+    x = j*LARGEUR_CASE
+    y = i*HAUTEUR_CASE
+    canvas.delete(fourmi)
+    c_fourmi(x,y,dir)
     return (i,j), dir
     
 
@@ -175,9 +158,10 @@ def play():
     """initialise la fourmi au milieu
     lance l'animation
     (fonction inspirée de http://pascal.ortiz.free.fr/contents/tkinter/projets_tkinter/langton/langton.html)"""
-    global pos, dir, p
+    global pos, dir, p, compt
     if p:
         pos, dir = modif_case(pos, dir, id) #on stocke les nouvelles valeurs
+        compt()
         canvas.after(vitesse, play)
     else:
         p = True
@@ -186,6 +170,11 @@ def pause():
     """permet d'arreter le programme"""
     global p
     p = False
+
+def avance():
+    global pos, dir, compt
+    pos, dir = modif_case(pos, dir, id)
+    compt()
 
 def sauvegarde():
     """Ecrit la taille de la grille et les valeurs de la variable
@@ -212,8 +201,10 @@ def load():
     N = int(a[0])
     pos = (int(a[1]), int(a[2]))
     dir = a[3]
-    canvas.delete()
-    c_fourmi()
+    canvas.delete("all")
+    x = int(a[2])*LARGEUR_CASE
+    y = int(a[1])*HAUTEUR_CASE
+    c_fourmi(x,y, dir)
     i = j = 0
     for ligne in fic:
         id[i][j] = int(ligne)
@@ -241,6 +232,12 @@ def aug_vitesse():
     else:
         vitesse = 10
 
+def compt():
+    global cmp
+    cmp+=1
+    label = tk.Label(text=str(cmp), padx=10, font='Times')
+    label.grid(row=0, column=7)
+
 ############################
 # programme principal
 
@@ -248,27 +245,31 @@ racine = tk.Tk()
 racine.title("Fourmi de Langton")
 
 # définition des widgets
-canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
+canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR, bg='snow')
 
-bouton_play = tk.Button(racine, text='play', command=play)
-bouton_pause = tk.Button(racine, text='pause', command=pause)
-bouton_aug = tk.Button(racine, text='accelerer', command=aug_vitesse)
-bouton_dim = tk.Button(racine, text='ralentir', command=dim_vitesse)
-bouton_save = tk.Button(racine, text='sauvegarder', command=sauvegarde)
-bouton_load = tk.Button(racine, text='ancienne configuration', command=load)
+bouton_play = tk.Button(racine, text='play',font='Times', command=play)
+bouton_pause = tk.Button(racine, text='pause',font='Times', command=pause)
+bouton_avance = tk.Button(racine, text='>>',font='Times', command = avance)
+bouton_recule = tk.Button(racine, text='<<',font='Times')
+bouton_aug = tk.Button(racine, text='accelerer',font='Times', command=aug_vitesse)
+bouton_dim = tk.Button(racine, text='ralentir',font='Times', command=dim_vitesse)
+bouton_save = tk.Button(racine, text='sauvegarder',font='Times', relief="flat", command=sauvegarde)
+bouton_load = tk.Button(racine, text='ancienne configuration',font='Times', relief="flat", command=load)
 
 
 
 #position des widgets
-canvas.grid(row=0, column=1, columnspan=5, rowspan=2)
+canvas.grid(row=0, column=1, columnspan=6, rowspan=2)
 bouton_play.grid(row=2, column=1)
 bouton_pause.grid(row=2, column=2)
+bouton_avance.grid(row=2, column=3)
+bouton_recule.grid(row=2, column=4)
 bouton_aug.grid(row=2, column=5)
 bouton_dim.grid(row=2, column=6)
 bouton_save.grid(row=0, column=0)
 bouton_load.grid(row=1, column=0)
 
-c_fourmi()
+c_fourmi(X,Y,"N")
 
 #boucle principale 
 racine.mainloop()
