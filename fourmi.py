97e05b023d0879d5ea1da630fe6e3ecb6a08c09b
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-05-09 03:14:28 +0200

    updates deuxieme mode de jeu

--- fourmi.py
+++ fourmi.py
@@ -6,12 +6,6 @@
 # https://github.com/uvsq-info/l1-python
 #########################################
 
-"""->pour deplacer la fourmi, faire une liste avec tous les deplacements
-possibles style : [[1,0],[0,1],[-1,0],[0,-1]] qui correspond respectivement
-à avancer d'une case vers la droite, vers le bas, vers la gauche, vers le
-haut
-et ensuite faire des if pour chaque condition (couleur) et definir l'indice"""
-
 #########################
 # import des librairies
 import tkinter as tk
@@ -21,7 +15,7 @@ from tracemalloc import stop
 #########################
 
 # taille de la grille carrée
-N = 10
+N = 50
 # dimensions du canvas et de la grille
 LARGEUR = 500
 HAUTEUR = 500
@@ -36,12 +30,16 @@ vitesse = 1000
 fourmi = None
 p = True #variable pour relancer le jeu apres avoir mis sur pause
 cmp = 0 #compte le nombre d'étapes réalisées
+mode2 = False #indique si on joue avec le mode 4 couleurs (deuxieme mode) ou le mode classique à 2 couleurs
 
 #position initiale au milieu du canvas orienté Nord
 X = LARGEUR_CASE*N//2
 Y = HAUTEUR_CASE*N//2
 pos = (N//2,N//2)
 dir = "N"
+
+#initialisation de la grille blanche = 0
+id = [[0]*(N) for k in range(N)] 
 
 ############################
 # fonction
@@ -57,9 +55,7 @@ def creation_case(i,j):
 def c_fourmi(x, y, dir):
     """- cree la liste id à la bonne taille
     - fait apparaitre la fourmi au milieu du tableau, orientée Nord"""
-    global id, fourmi
-    id = [[0]*(N) for k in range(N)] #initialisation de la grille blanche = 0
-
+    global fourmi
     if dir == "N":
         fourmi = canvas.create_polygon((x+LARGEUR_CASE//2,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x,y+HAUTEUR_CASE),
                                     fill='steelblue3')
@@ -72,6 +68,7 @@ def c_fourmi(x, y, dir):
     elif dir == "O":
         fourmi = canvas.create_polygon((x,y+HAUTEUR_CASE//2), (x+LARGEUR_CASE,y+HAUTEUR_CASE), (x+LARGEUR_CASE,y),
                                             fill='steelblue3')
+    return fourmi
 
 
 def mouvement(pos, dir, id):
@@ -134,9 +131,9 @@ def mouvement(pos, dir, id):
     x = j*LARGEUR_CASE
     y = i*HAUTEUR_CASE
     canvas.delete(fourmi)
-    c_fourmi(x,y,dir)
+    fourmi = c_fourmi(x,y,dir)
     return (i,j), dir
-    
+
 
 def modif_case(pos, dir, id):
     """- modifie l'etat de l'ancienne case apres que la fourmi l'ai quitte
@@ -147,22 +144,60 @@ def modif_case(pos, dir, id):
     i,j = pos
     case = id[i][j]
     if case == 0:
-        case = creation_case(i,j)
-        id[i][j] = case
+        case = creation_case(i,j) #cree l'id qui correspond au nouvel etat de la case
+        id[i][j] = case #maj du tableau 
     else:
         canvas.delete(case)
         id[i][j] = 0
     return (ni,nj), ndir
 
+def modif_case2(pos, dir, id):
+    """meme principe que modif_cases() mais avec 4couleurs:
+    - id = 0 blanc (droite) -> id = 1 vert (droite) -> id = 2 rose (gauche) -> id = 3 noir (gauche)"""
+    (ni,nj), ndir = mouvement(pos, dir, id)
+    i,j = pos
+    x = j*LARGEUR_CASE
+    y = i*HAUTEUR_CASE
+    case = id[i][j]
+    if id[i][j] == 0:
+        case = canvas.create_rectangle((x,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE),
+                                    fill='chartreuse2')
+        id[i][j] = 1
+    elif id[i][j] == 1:
+        case = canvas.create_rectangle((x,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE),
+                                    fill='turquoise1')
+        id[i][j] = 2
+    elif id[i][j] == 2:
+        case = canvas.create_rectangle((x,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE),
+                                    fill='gold')
+        id[i][j] = 3
+    else:
+        canvas.delete(case)
+        id[i][j] = 0
+    return (ni,nj), ndir
+   
+
 def play():
     """initialise la fourmi au milieu
     lance l'animation
     (fonction inspirée de http://pascal.ortiz.free.fr/contents/tkinter/projets_tkinter/langton/langton.html)"""
-    global pos, dir, p, compt
+    global pos, dir, p, mode2
+    mode2 = False
     if p:
         pos, dir = modif_case(pos, dir, id) #on stocke les nouvelles valeurs
         compt()
         canvas.after(vitesse, play)
+    else:
+        p = True
+
+def play2():
+    """meme fonction que play() mais pour le deuxieme mode de jeu : 4 couleurs"""
+    global pos, dir, p, mode2
+    mode2 = True
+    if p:
+        pos, dir = modif_case2(pos, dir, id) #on stocke les nouvelles valeurs
+        compt()
+        canvas.after(vitesse, play2)
     else:
         p = True
 
@@ -172,8 +207,12 @@ def pause():
     p = False
 
 def avance():
+    """fait avancer etape par etape la fourmi"""
     global pos, dir, compt
-    pos, dir = modif_case(pos, dir, id)
+    if mode2:
+        pos, dir = modif_case2(pos, dir, id)
+    else:
+        pos, dir = modif_case(pos, dir, id)
     compt()
 
 def sauvegarde():
@@ -183,36 +222,54 @@ def sauvegarde():
      """
     fic = open("sauvegarde.txt", "w")
     i, j = pos
-    fic.write(str(N) + " " + str(i) + " " + str(j) + " "+ str(dir) + "\n")
+    fic.write(str(N) +" "+ str(i) +" "+ str(j) +" "+ str(dir) +" "+str(cmp) +" "+ str(mode2) + "\n")
     for i in range(N):
         for j in range(N):
             fic.write(str(id[i][j]) + "\n")
     fic.close()
 
 def load():
-    """
-    Lire le fichier sauvegarde.txt et affiche dans le canvas la configuration lu
+    """ Lit le fichier sauvegarde.txt et affiche dans le canvas la configuration lu
      -> fonction provenant du code generation_terrain fait en cours
     """
-    global N, id, pos, dir
+    global N, id, pos, dir, p, cmp, case, mode2
     fic = open("sauvegarde.txt", "r") 
     l1 = fic.readline()
     a = l1.split()
+
     N = int(a[0])
     pos = (int(a[1]), int(a[2]))
     dir = a[3]
+    cmp = int(a[4])
+    mode2 = bool(a[5])
+
     canvas.delete("all")
     x = int(a[2])*LARGEUR_CASE
     y = int(a[1])*HAUTEUR_CASE
-    c_fourmi(x,y, dir)
-    i = j = 0
+    c_fourmi(x,y, dir) # placer fourmi avec orientation
+
+    i = j = 0 # recreer terrain
     for ligne in fic:
         id[i][j] = int(ligne)
+        if mode2:
+            x = j*LARGEUR_CASE
+            y = i*HAUTEUR_CASE
+            if id[i][j] == 1:
+                case = canvas.create_rectangle((x,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE),
+                                    fill='chartreuse2')
+            elif id[i][j] == 2:
+                case = canvas.create_rectangle((x,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE),
+                                            fill='turquoise1')
+            elif id[i][j] == 3:
+                case = canvas.create_rectangle((x,y), (x+LARGEUR_CASE,y+HAUTEUR_CASE),
+                                            fill='gold')
+        else:
+            if id[i][j]!=0:
+                creation_case(i,j)
         j += 1
         if j == N:
             j = 0
             i += 1
-    print(id)
     p = True
     fic.close()
 
@@ -233,10 +290,22 @@ def aug_vitesse():
         vitesse = 10
 
 def compt():
+    """affiche un compteur avec le nombre d'etapes effectuees"""
     global cmp
     cmp+=1
     label = tk.Label(text=str(cmp), padx=10, font='Times')
     label.grid(row=0, column=7)
+
+def redemarrer():
+    """reinitialise l'affichage"""
+    global id, cmp, pos, dir
+    pos = (N//2,N//2)
+    dir = "N"
+    cmp = 0
+    compt()
+    canvas.delete("all")
+    id = [[0]*(N) for k in range(N)]
+    c_fourmi(X,Y,dir)
 
 ############################
 # programme principal
@@ -255,8 +324,9 @@ bouton_aug = tk.Button(racine, text='accelerer',font='Times', command=aug_vitess
 bouton_dim = tk.Button(racine, text='ralentir',font='Times', command=dim_vitesse)
 bouton_save = tk.Button(racine, text='sauvegarder',font='Times', relief="flat", command=sauvegarde)
 bouton_load = tk.Button(racine, text='ancienne configuration',font='Times', relief="flat", command=load)
+bouton_redem = tk.Button(racine, text='redemarrer',font='Times', relief="flat", command=redemarrer)
 
-
+bouton_play2 = tk.Button(racine, text='deuxieme mode de jeu',font='Times', command=play2)
 
 #position des widgets
 canvas.grid(row=0, column=1, columnspan=6, rowspan=2)
@@ -268,6 +338,9 @@ bouton_aug.grid(row=2, column=5)
 bouton_dim.grid(row=2, column=6)
 bouton_save.grid(row=0, column=0)
 bouton_load.grid(row=1, column=0)
+bouton_redem.grid(row=2, column=0)
+
+bouton_play2.grid(row=1, column=7)
 
 c_fourmi(X,Y,"N")
 
